2ea54dc062fcdddf5e8176c8e0affedaa9992132
masahiro96848 <feetwcpic9436@gmail.com>
2022-12-14 18:23:48 +0900

    TodoItemコンポーネントを作成

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,8 +1,16 @@
 <template>
-    <li v-for="todo in todos" class="list">
-        <span class="item">{{ todo }}</span>
-        <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
-    </li>
+    <section class="container-area">
+        <ul class="list-row">
+            <li v-for="todo in filterTodoItems" class="list">
+                <span class="item">{{ todo.text }}</span>
+                <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
+            </li>
+        </ul>
+    </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFilterValue } from '../composables/useFilterValue'
+
+const { filterTodoItems } = useFilterValue()
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,5 @@
 <template>
-    <section class="container-area">
-        <ul class="list-row">
-            <li v-for="todo in filterTodoItems" class="list">
-                <span class="item">{{ todo.text }}</span>
-                <i class="fa fa-trash fa-lg" aria-hidden="true"></i>
-            </li>
-        </ul>
-    </section>
+    <TodoItem />
 </template>
 
 <script setup lang="ts">
