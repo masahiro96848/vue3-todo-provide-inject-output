b292ec2aba80ae4ebb6ba5f887e96319b9cf0a04
masahiro96848 <feetwcpic9436@gmail.com>
2022-12-14 18:21:26 +0900

    AddTodoをリファクタリング

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,13 @@
 <template>
     <div class="container">
         <h1 class="title">Todo List</h1>
-        <AddTodo v-model:todo="todo" @add-todo="handleAddTodo" />
+        <AddTodo />
         <!-- todoListのエリア -->
         <TodoList />
     </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, ref, provide } from 'vue'
 import AddTodo from './components/ AddTodo.vue'
 import TodoList from './components/TodoList.vue'
-import { Todo } from './interface/Todo'
-
-const state = reactive<Todo>({
-    todo: '',
-    todos: [],
-})
-
-const todo = ref('')
-const todos = ref<Array<string>>([])
-
-const handleAddTodo = () => {
-    if (todo.value === '') {
-        return
-    }
-    todos.value.push(todo.value)
-    todo.value = ''
-}
 </script>
--- src/components/ AddTodo.vue
+++ src/components/ AddTodo.vue
@@ -3,7 +3,6 @@
         <form action="" @submit.prevent="addTodoFunc">
             <input
                 v-model="inputValue"
-                @input="onInputTodo"
                 class="input-area"
                 placeholder="Todoを追加"
                 type="text"
@@ -13,17 +12,8 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useTodo } from '../store/todo/useTodo'
 import { useInputValue } from '../composables/useInputValue'
-
-interface Props {
-    todo: string
-}
-
-interface Emits {
-    (event: 'update:todo', todo: string): void
-}
 
 const { inputValue } = useInputValue()
 const { addTodo } = useTodo()
@@ -31,19 +21,5 @@ const { addTodo } = useTodo()
 const addTodoFunc = () => {
     addTodo(inputValue.value)
     inputValue.value = ''
-}
-
-const props = defineProps<Props>()
-
-const emit = defineEmits<Emits>()
-
-const handleAddTodo = (): void => {
-    emit('addTodo')
-}
-
-const onInputTodo = (event: Event): void => {
-    const element = event.target as HTMLInputElement
-    const inputTodo = String(element.value)
-    emit('update:todo', inputTodo)
 }
 </script>
